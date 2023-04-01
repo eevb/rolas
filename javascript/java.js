fa9831facd60b9efaa2e37e610b74961cf80483d
eevb <96337431+eevb@users.noreply.github.com>
2023-04-01 20:34:27 +0200

    update for login

--- javascript/java.js
+++ javascript/java.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+/*var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
@@ -23,4 +23,24 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
+}
+*/
+
+//!!!!!!!!!!!!!!!!!! log in verification code !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+// Get the token value from local storage
+const tokenValue = localStorage.getItem("token");
+
+// Check if the token value is "1234"
+if (tokenValue === "12345") {
+  // If the token is "1234", show the element with ID "rolas1"
+  const rolas1Element = document.getElementById("rolas1");
+  rolas1Element.style.display = "block";
+} else {
+  // If the token is not "1234", remove the element with ID "rolas1"
+  const rolas1Element = document.getElementById("rolas1");
+  rolas1Element.parentNode.removeChild(rolas1Element);
+  // Redirecting to 
+  window.location.href = 'https://eevb.github.io/login-website';
+
 }
